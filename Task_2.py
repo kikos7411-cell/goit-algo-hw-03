f755aa8b5c80f3dade1dd166db556b33de419c0f
kikos7411-cell <kikos7411@gmail.com>
2025-10-19 15:28:21 +0300

    v_2

--- Task_2.py
+++ Task_2.py
@@ -1,10 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    numbers = set()
-    while len(numbers) < quantity:
-        numbers.add(random.randint(min,max))
-    return (numbers)
+    if not(1 <= min <= max <= 1000) or not (min <= quantity <= max):
+        return []
+    else:
+        numbers = set()
+        while len(numbers) < quantity:
+            numbers.add(random.randint(min, max))
+        return sorted(numbers)
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+
+lottery_numbers = get_numbers_ticket(1, 1000, 64)
 print("Лоторейні номера:", lottery_numbers)
