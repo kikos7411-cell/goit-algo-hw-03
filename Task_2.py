5ae083a1d4b5b915b4f74037bbb5b49a3e85ac32
kikos7411-cell <kikos7411@gmail.com>
2025-10-21 15:38:00 +0300

    Task_2_v5.py

--- Task_2.py
+++ Task_2.py
@@ -4,11 +4,7 @@ def get_numbers_ticket(min, max, quantity):
     if not(1 <= min <= max <= 1000) or not (1 <= quantity <= max) \
         or not ((max - min + 1) >= quantity):
         return []
-    else:
-        numbers = set()
-        while len(numbers) < quantity:
-            numbers.add(random.randint(min, max))
-        return sorted(numbers)
+    return sorted(random.sample(range(min, max + 1), quantity))
 
 
 lottery_numbers = get_numbers_ticket(980, 999, 11)
